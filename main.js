f26505ed237ec5bca4d4ff5a132b69d2a20fd634
Instinct <61635505+uinstinct@users.noreply.github.com>
2022-11-27 16:54:29 +0000

    feat: use magnets.txt

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
 import WebTorrent from 'webtorrent';
 import cliProgress from 'cli-progress';
-
-const client = new WebTorrent()
-
-const magnetURI = 'magnet:?xt=urn:btih:CF6F1F769B23994C995FF0EF1B5510900769AE57&dn=My+Hero+Academia%3A+Two+Heroes+%282018%29+%5BBluRay%5D+%5B1080p%5D+%5BYTS%5D+%5BYIFY%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.com%3A2710%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce'
-
-
+import fs from 'fs';
 
 const bar1 = new cliProgress.SingleBar({
-    format: '{bar} | {percentage}% || Speed: {speed}'
+    format: '{bar} | {percentage}% || Speed: {speed} MB/s || Downloaded: {downloaded} MB || Name: {torrentName}'
 }, cliProgress.Presets.shades_classic);
+const client = new WebTorrent();
 
+const convertMbps = (byts) => byts / 1_000_000;
 
-client.add(magnetURI, { path: './' }, (torrent) => {
-    bar1.start(100, 0);
+function startDownloadingMagnetLink(maglink) {
+    console.log('downloading --> ', maglink, '\n');
 
-    const interval = setInterval(() => {
-        bar1.update((torrent.progress * 100).toFixed(1), { speed: torrent.downloadSpeed });
-    }, 1_000);
+    client.add(maglink, { path: './torrent-downloads/' }, (torrent) => {
+        bar1.start(100, 0);
 
-    torrent.on('done', () => {
-        bar1.stop();
-        clearInterval(interval);
-        console.log('torrent download finished');
-        process.exit(0);
+        const interval = setInterval(() => {
+            bar1.update(torrent.progress * 100, { speed: convertMbps(torrent.downloadSpeed), downloaded: convertMbps(torrent.downloaded), torrentName: torrent.name });
+        }, 1_000);
+
+        torrent.on('done', () => {
+            bar1.stop();
+            clearInterval(interval);
+            console.log('torrent download finished\n');
+            process.exit(0);
+        });
     });
-});
+}
+
+const magnetLinks = fs.readFileSync('./magnets.txt')?.toString().split('\n') || [];
+magnetLinks.forEach(maglink => startDownloadingMagnetLink(maglink));
