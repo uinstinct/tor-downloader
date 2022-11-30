f20b2140bfbb82f92be2a72de6f8db3874188bbf
Instinct <61635505+uinstinct@users.noreply.github.com>
2022-11-30 19:23:18 +0000

    chore: format bar2 progress

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,9 @@ import cliProgress from 'cli-progress';
 
 export const convertMbps = (byts) => byts / 1_000_000;
 
-export const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+export const bar1 = new cliProgress.SingleBar({
+    format: '{bar} {percentage}%',
+}, cliProgress.Presets.shades_classic);
 
 export const bar2 = new cliProgress.SingleBar({
     format: 'Speed: {speed} MB/s || Downloaded: {downloaded} MB || No. of Peers: {peers} || Name: {torrentName}'
