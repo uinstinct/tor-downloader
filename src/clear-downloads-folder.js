5b76d29b920d4c29117f40ac42d2d0c774b88bb7
Instinct <61635505+uinstinct@users.noreply.github.com>
2022-11-30 19:22:11 +0000

    fix: clear downloads folders problems

--- src/clear-downloads-folder.js
+++ src/clear-downloads-folder.js
@@ -1,10 +1,16 @@
 import fs from 'fs';
 
+import { DOWNLOADS_FOLDER } from './constants.js';
+
 function clearDownloadsFolder() {
-    console.log(fs.existsSync('./torrent-downloads'), '<-- result');
-    // const files = fs.readdirSync('./torrent-downloads');
-    
-    // console.log(files);
+    // console.log(fs.existsSync('./torrent-downloads'), '<-- result');
+    const files = fs.readdirSync(DOWNLOADS_FOLDER);
+
+    files.forEach(file => {
+        if (file !== '.gitkeep') {
+            fs.rmSync(DOWNLOADS_FOLDER + '/' + file, { recursive: true, force: true });
+        }
+    });
 }
 
 clearDownloadsFolder();
--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,6 @@
 // progress bar
 export const PROGRESS_BAR_INTERVAL = 4_000;
 export const PROGRESS_BAR_SWITCH_TIME = 2_000;
+
+// folders
+export const DOWNLOADS_FOLDER = './torrent-downloads';
