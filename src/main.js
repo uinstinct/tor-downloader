70bf0f961e25e7816bb5f5d633b7baa050bcbafb
Instinct <61635505+uinstinct@users.noreply.github.com>
2022-12-02 20:29:31 +0000

    feat: working of upload of any file to google drive

--- src/main.js
+++ src/main.js
@@ -1,42 +1,97 @@
 import WebTorrent from 'webtorrent';
 import fs from 'fs';
+import mime from 'mime-types';
 
 import { bar1, bar2, convertMbps } from './utils.js';
-import { PROGRESS_BAR_INTERVAL, PROGRESS_BAR_SWITCH_TIME } from './constants.js'
+import { DOWNLOADS_FOLDER, PROGRESS_BAR_INTERVAL, PROGRESS_BAR_SWITCH_TIME } from './constants.js'
+import getDriveClient from './upload-to-drive.js';
 
 const client = new WebTorrent();
 
-function startDownloadingMagnetLink(maglink) {
-    console.log('downloading --> ', maglink, '\n');
+async function startDownloadingMagnetLink(maglink) {
+    return new Promise((resolve, reject) => {
+        console.log('downloading --> ', maglink, '\n');
 
-    client.add(maglink, { path: './torrent-downloads/' }, (torrent) => {
-        bar1.start(100, 0);
-        bar2.start(100, 100);
+        client.add(maglink, { path: DOWNLOADS_FOLDER }, (torrent) => {
+            bar1.start(100, 0);
+            bar2.start(100, 100);
 
-        const interval1 = setInterval(() => {
-            bar1.update(torrent.progress * 100);
-        }, PROGRESS_BAR_INTERVAL);
-
-        let interval2;
-
-        setTimeout(() => {
-            interval2 = setInterval(() => {
-                bar2.update(100, { speed: convertMbps(torrent.downloadSpeed).toFixed(1), downloaded: convertMbps(torrent.downloaded).toFixed(0), peers: torrent.numPeers, torrentName: torrent.name });
+            const interval1 = setInterval(() => {
+                bar1.update(torrent.progress * 100);
             }, PROGRESS_BAR_INTERVAL);
-        }, PROGRESS_BAR_SWITCH_TIME);
 
-        torrent.on('done', () => {
-            bar1.stop();
-            bar2.stop();
+            let interval2;
 
-            clearInterval(interval1);
-            clearInterval(interval2);
+            setTimeout(() => {
+                interval2 = setInterval(() => {
+                    bar2.update(100, { speed: convertMbps(torrent.downloadSpeed).toFixed(1), downloaded: convertMbps(torrent.downloaded).toFixed(0), peers: torrent.numPeers, torrentName: torrent.name });
+                }, PROGRESS_BAR_INTERVAL);
+            }, PROGRESS_BAR_SWITCH_TIME);
 
-            console.log('torrent download finished\n');
-            process.exit(0);
+            torrent.on('done', () => {
+                bar1.stop();
+                bar2.stop();
+
+                clearInterval(interval1);
+                clearInterval(interval2);
+
+                console.log('torrent download finished\n');
+
+                resolve(torrent.name);
+            });
+
+            torrent.on('error', (err) => {
+                console.log('\n the error was ', err);
+                reject(null);
+            });
         });
     });
 }
 
-const magnetLinks = fs.readFileSync('./magnets.txt')?.toString().split('\n') || [];
-magnetLinks.forEach(maglink => startDownloadingMagnetLink(maglink));
+/**
+ * @param {ReturnType<typeof import('./upload-to-drive').default>} driveClient
+ * @param {string} filePath
+ * @param {string} parentFolderId
+ */
+async function uploadContentsInFolder(driveClient, filePath, parentFolderId) {
+    const dirents = fs.readdirSync(filePath, { withFileTypes: true });
+    for (const dirent of dirents) { // upload files/folders one by one
+        const currentFilePath = filePath + '/' + dirent.name;
+        if (dirent.isDirectory()) {
+            console.log('was directory', dirent.name);
+            const { id: currentFolderId } = await driveClient.createFolder(dirent.name, parentFolderId);
+            await uploadContentsInFolder(driveClient, currentFilePath, currentFolderId);
+        } else {
+            console.log('was file', dirent.name);
+            await driveClient.saveFile(dirent.name, currentFilePath, mime.contentType(dirent.name), parentFolderId);
+        }
+    };
+}
+
+async function main() {
+    console.log('Initializing drive client ...');
+    const driveClient = getDriveClient();
+    console.log('Checking if gdrive folder is present?')
+    let folder = await driveClient.searchFolder();
+    if (!folder) {
+        console.log('Creating download folder on drive >');
+        folder = await driveClient.createFolder();
+    } else {
+        console.log('Download Folder present on drive!');
+    }
+    console.log(`Folder Details :: ID: ${folder.id} | Name: ${folder.name}`);
+
+    const magnetLinks = fs.readFileSync('./magnets.txt')?.toString().split('\n') || [];
+
+    // const torrentName = await startDownloadingMagnetLink(magnetLinks.at(0));
+    // if (!torrentName) {
+    //     console.log('torrent could not be downloaded');
+    //     return ;
+    // }
+
+    await uploadContentsInFolder(driveClient, DOWNLOADS_FOLDER, folder.id);
+
+    process.exit(0);
+}
+
+main();
--- src/upload-to-drive.js
+++ src/upload-to-drive.js
@@ -1,5 +1,8 @@
+import fs from 'fs';
 import { google } from 'googleapis';
-import { CLIENT_ID, CLIENT_SECRET, DRIVE_DOWNLOAD_FOLDER, REDIRECT_URI, REFRESH_TOKEN } from './constants.js'
+
+import { CLIENT_ID, CLIENT_SECRET, DRIVE_DOWNLOAD_FOLDER, REDIRECT_URI, REFRESH_TOKEN } from './constants.js';
+import { bar1 } from './utils.js'
 
 /**
  * @typedef {{id: string; name: string;}} PartialDriveClient
@@ -12,47 +15,58 @@ export default function getDriveClient() {
 
     /**
      * @param {string} folderName 
+     * @param {string?} folderId
      * @returns {Promise<PartialDriveClient>}
      */
-    const createFolder = async (folderName = DRIVE_DOWNLOAD_FOLDER) => {
+    const createFolder = async (folderName = DRIVE_DOWNLOAD_FOLDER, folderId) => {
         return driveClient.files.create({
             resource: {
                 name: folderName,
                 mimeType: 'application/vnd.google-apps.folder',
+                parents: folderId ? [folderId] : [],
             },
             fields: 'id,name'
-        });
+        }).then(response => response.data);
     };
 
     /**
      * @param {string} folderName 
      * @returns {Promise<PartialDriveClient|null>}
      */
-    const searchFolder = async (folderName = DRIVE_DOWNLOAD_FOLDER) => {
-        driveClient.files.list({
-            q: `mimeType='application/vnd.google-apps.folder' and name='${folderName}'`,
-            fields: 'files(id, name)',
-        }).then((response) => response.data.files ? response.data.files.at(0) : null);
-    }
+    const searchFolder = async (folderName = DRIVE_DOWNLOAD_FOLDER) => driveClient.files.list({
+        q: `mimeType='application/vnd.google-apps.folder' and name='${folderName}'`,
+        fields: 'files(id, name)',
+    }).then((response) => response.data.files ? response.data.files.at(0) : null)
 
     /**
      * @param {string} fileName 
      * @param {string} filePath 
      * @param {string} fileMimeType 
-     * @param {PartialDriveClient['id']} folderId 
-     * @returns {PartialDriveClient}
+     * @param {PartialDriveClient['id']?} folderId 
+     * @returns {Promise<PartialDriveClient>}
      */
-    const saveFile = async (fileName, filePath, fileMimeType, folderId) => driveClient.files.create({
-        requestBody: {
-            name: fileName,
-            mimeType: fileMimeType,
-            parents: folderId ? [folderId] : [],
-        },
-        media: {
-            mimeType: fileMimeType,
-            body: fs.createReadStream(filePath),
-        },
-    });
+    const saveFile = async (fileName, filePath, fileMimeType, folderId) => {
+        console.log(`Uploading ${fileName} to Google Drive`);
+        bar1.start();
+
+        return driveClient.files.create({
+            requestBody: {
+                name: fileName,
+                mimeType: fileMimeType,
+                parents: folderId ? [folderId] : [],
+            },
+            media: {
+                mimeType: fileMimeType,
+                body: fs.createReadStream(filePath),
+            },
+
+        }, {
+            onUploadProgress: (event) => bar1.update((event.bytesRead / fs.statSync(filePath).size) * 100)
+        }).then(val => {
+            bar1.stop();
+            return val;
+        });
+    }
 
     return {
         createFolder,
@@ -60,16 +74,3 @@ export default function getDriveClient() {
         saveFile
     };
 }
-
-// async function run() {
-//     const client = new google.auth.OAuth2({ clientId: CLIENT_ID, clientSecret: CLIENT_SECRET, redirectUri: REDIRECT_URI });
-//     client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
-//     const driveClient = google.drive({ version: 'v3', auth: client });
-
-//     const res = await driveClient.files.list({ pageSize: 10 });
-
-//     console.log(res.data.files);
-// }
-
-// run();
